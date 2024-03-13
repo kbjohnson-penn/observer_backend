debd6c7f4b58ade33ec4d736a6aac867288f1be0
Sriharsha Mopidevi <31341894+sriharshamvs@users.noreply.github.com>
2024-03-13 09:01:24 -0400

    feat: add serializers & views for graph models

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,25 +1,53 @@
 from rest_framework import serializers
 from .models import Patient, Provider, Department, MultiModalDataPath, Encounter
 
+
+class GraphPatientSerializer(serializers.Serializer):
+    race = serializers.CharField()
+    ethnicity = serializers.CharField()
+    date_of_birth = serializers.DateField()
+    sex = serializers.CharField()
+
+
+class GraphProviderSerializer(serializers.Serializer):
+    race = serializers.CharField()
+    ethnicity = serializers.CharField()
+    date_of_birth = serializers.DateField()
+    sex = serializers.CharField()
+
+
+class GraphDepartmentSerializer(serializers.Serializer):
+    name = serializers.CharField()
+
+
+class GraphEncounterSerializer(serializers.Serializer):
+    case_id = serializers.CharField()
+    encounter_date = serializers.DateField()
+
+
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = '__all__'
+
 
 class ProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provider
         fields = '__all__'
 
+
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
 
+
 class MultiModalDataPathSerializer(serializers.ModelSerializer):
     class Meta:
         model = MultiModalDataPath
         fields = '__all__'
+
 
 class EncounterSerializer(serializers.ModelSerializer):
     class Meta:
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,58 @@ from rest_framework.response import Response
 
 from .models import Patient, Provider, Department, MultiModalDataPath, Encounter
 from .serializers import PatientSerializer, ProviderSerializer, DepartmentSerializer, MultiModalDataPathSerializer, EncounterSerializer
+from .serializers import GraphPatientSerializer, GraphProviderSerializer, GraphDepartmentSerializer, GraphEncounterSerializer
+from neomodel import db
+
+
+class GraphPatientViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GraphPatientSerializer
+
+    def get_queryset(self):
+        query = "MATCH (n:GraphPatient) RETURN n"
+        results, meta = db.cypher_query(query)
+        return [dict(node[0]) for node in results]
+
+
+class GraphProviderViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GraphProviderSerializer
+
+    def get_queryset(self):
+        query = "MATCH (n:GraphProvider) RETURN n"
+        results, meta = db.cypher_query(query)
+        return [dict(node[0]) for node in results]
+
+
+class GraphDepartmentViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GraphDepartmentSerializer
+
+    def get_queryset(self):
+        query = "MATCH (n:GraphDepartment) RETURN n"
+        results, meta = db.cypher_query(query)
+        return [dict(node[0]) for node in results]
+
+
+class GraphEncounterViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GraphEncounterSerializer
+
+    def get_queryset(self):
+        query = "MATCH (n:GraphEncounter) RETURN n"
+        results, meta = db.cypher_query(query)
+        return [dict(node[0]) for node in results]
+
+
+class DepartmentEncountersView(APIView):
+    def get(self, request, department_name):
+        query = """
+        MATCH (e:GraphEncounter)-[:BELONGS_TO]->(d:GraphDepartment)
+        WHERE d.name = $department_name
+        RETURN e
+        """
+        params = {'department_name': department_name}
+        results, meta = db.cypher_query(query, params)
+        encounters = [dict(node[0]) for node in results]
+        serializer = GraphEncounterSerializer(encounters, many=True)
+        return Response(serializer.data)
 
 
 class PatientViewSet(viewsets.ModelViewSet):
