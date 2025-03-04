582164f3b5f9fe9bb9264b2ad92168ae8ae85bf0
Sriharsha Mopidevi <sriharshamvs@gmail.com>
2025-03-04 11:11:53 -0500

    feat: Update address fields in Profile and Organization models; add address_1 and address_2

--- dashboard/forms/profile_forms.py
+++ dashboard/forms/profile_forms.py
@@ -10,5 +10,5 @@ class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ['date_of_birth', 'phone_number', 'address',
+        fields = ['date_of_birth', 'phone_number', 'address_1', 'address_2',
                   'city', 'state', 'country', 'zip_code', 'bio', 'organization', 'tier']
--- dashboard/models/profile_models.py
+++ dashboard/models/profile_models.py
@@ -19,12 +19,18 @@ class Tier(models.Model):
 
 class Organization(models.Model):
     name = models.CharField(max_length=100, validators=[validate_name])
-    address = models.TextField(blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True, validators=[validate_name])
-    state = models.CharField(max_length=100, blank=True, null=True, validators=[validate_name])
-    country = models.CharField(max_length=100, blank=True, null=True, validators=[validate_name])
-    zip_code = models.CharField(max_length=5, blank=True, null=True, validators=[validate_zip_code])
-    phone_number = models.CharField(max_length=12, blank=True, null=True, validators=[validate_phone_number])
+    address_1 = models.CharField(max_length=100, blank=True, null=True)
+    address_2 = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True,
+                            null=True, validators=[validate_name])
+    state = models.CharField(max_length=100, blank=True,
+                             null=True, validators=[validate_name])
+    country = models.CharField(
+        max_length=100, blank=True, null=True, validators=[validate_name])
+    zip_code = models.CharField(
+        max_length=5, blank=True, null=True, validators=[validate_zip_code])
+    phone_number = models.CharField(
+        max_length=12, blank=True, null=True, validators=[validate_phone_number])
     website = models.URLField(blank=True, null=True)
 
     def __str__(self):
@@ -34,17 +40,23 @@ class Organization(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    phone_number = models.CharField(max_length=12, blank=True, null=True, validators=[validate_phone_number])
+    phone_number = models.CharField(
+        max_length=12, blank=True, null=True, validators=[validate_phone_number])
     organization = models.ForeignKey(
         Organization, on_delete=models.CASCADE, null=True, blank=True)
     tier = models.ForeignKey(
         Tier, on_delete=models.CASCADE, null=True, blank=True)
-    address = models.TextField(blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True, validators=[validate_name])
-    state = models.CharField(max_length=100, blank=True, null=True, validators=[validate_name])
-    country = models.CharField(max_length=100, blank=True, null=True, validators=[validate_name])
-    zip_code = models.CharField(max_length=5, blank=True, null=True, validators=[validate_zip_code])
-    bio = models.TextField(blank=True, null=True)
+    address_1 = models.CharField(max_length=100, blank=True, null=True)
+    address_2 = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True,
+                            null=True, validators=[validate_name])
+    state = models.CharField(max_length=100, blank=True,
+                             null=True, validators=[validate_name])
+    country = models.CharField(
+        max_length=100, blank=True, null=True, validators=[validate_name])
+    zip_code = models.CharField(
+        max_length=5, blank=True, null=True, validators=[validate_zip_code])
+    bio = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
