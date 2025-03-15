b17c5d0738994e25d15f3879f8f7d5ba43e579ee
Sriharsha Mopidevi <sriharshamvs@gmail.com>
2025-03-15 16:54:20 -0400

    fix: Update MultiModalDataSerializer fields to include specific attributes

--- dashboard/api/serializers/encounter_serializers.py
+++ dashboard/api/serializers/encounter_serializers.py
@@ -5,7 +5,8 @@ from dashboard.models import Encounter, MultiModalData, Department, EncounterSou
 class MultiModalDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = MultiModalData
-        fields = '__all__'
+        fields = ["provider_view", "patient_view", "room_view", "audio", "transcript",
+                  "patient_survey", "provider_survey", "patient_annotation", "provider_annotation", "timestamp"]
 
 
 class EncounterFileSerializer(serializers.ModelSerializer):
